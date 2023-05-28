fcbf4b720e1fb80420647d360f0bb44458dec9b0
Deepak Raj <deepak@kali.deepak>
2023-05-28 21:25:41 +0530

    Changed description and title of html

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,16 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 
-
 import "./styles/App.scss";
 import "./styles/Var.scss";
-
-
-
-
 import "./styles/MediaQuery.scss"
-
 
 
 function App() {
   return (
     <>
-      
       <Header />
-
       <Main />
-
       <Footer />
     </>
   );
